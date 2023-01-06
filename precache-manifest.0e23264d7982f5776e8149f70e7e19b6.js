15ebb89155f20b02a036144da822f2e31f5c7e30
shivanikhedikar <khedikar.shivani@gmail.com>
2023-01-06 23:28:15 +0530

    Updates

---
+++ precache-manifest.0e23264d7982f5776e8149f70e7e19b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a543406166e6be154bff",
+    "url": "/TextUtils/static/css/main.56d94304.chunk.css"
+  },
+  {
+    "revision": "a543406166e6be154bff",
+    "url": "/TextUtils/static/js/main.a5434061.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "6d365031a6f308f4fbeb5317e3e8ea3f",
+    "url": "/TextUtils/index.html"
+  }
+];
